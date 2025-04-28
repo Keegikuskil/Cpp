5d2cae152a2b1fb2fe79bd1dad10b89c410d8a96
Keegikuskil <jannupoiss007@gmail.com>
2025-04-28 19:27:13 +0300

    Create Korrutis.cpp

---
+++ Korrutis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sum;
+int a, b, c;
+
+int main()  {
+    cout << "" ;
+    cin >> a >> b >> c;
+    sum = a * b * c;
+    cout << sum;
+    return 0;
+
+}
+
+
+
+
+    
