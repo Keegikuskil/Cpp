b5bd2593f8460da0916f4dcc037cf4c40437c239
Keegikuskil <jannupoiss007@gmail.com>
2025-04-28 20:14:20 +0300

    Update Liigaasta.cpp

--- Liigaasta.cpp
+++ Liigaasta.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int aasta;
+
+int main()  {
+    cout << "";
+    cin >> aasta;
+    if (aasta % 4 == 0 && aasta % 100 != 0 || aasta % 400 == 0) {
+        cout << "Jah";
+    }   else    {
+        cout << "Ei";
+    }
+    return 0;
+    
+};
