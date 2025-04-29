cf91de9450fdc870161663b7c00499cc1a630e60
Keegikuskil <jannupoiss007@gmail.com>
2025-04-29 19:37:05 +0300

    Create Ristsumma.cpp

---
+++ Ristsumma.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+void ristsumma()    {
+    int sum;
+    std::string sisend;
+    std::cin >> sisend;
+    for (int num: sisend)   {
+        sum = sum + num;
+    }
+    std::cout << sum;
+}
+
+
+int main()  {
+    ristsumma();
+}
