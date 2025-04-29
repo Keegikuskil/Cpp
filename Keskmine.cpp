354b110031d2e0904a3f77d9b9886b4574be02a6
Keegikuskil <jannupoiss007@gmail.com>
2025-04-29 08:49:25 +0300

    Update Keskmine.cpp

--- Keskmine.cpp
+++ Keskmine.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 using namespace std;
 
+int n;
+int sum;
+
 int main()  {
-    ifstream cin("input.in")
+    ifstream cin("input.in");
+    cin >> n;
+
+    for(int i = 0; i < n; i ++) {
+        int arv;
+        cin >> arv;
+        sum = sum + arv;
+    }
+    ofstream cout("output.out");
+    cout << round(sum / n);
 }
